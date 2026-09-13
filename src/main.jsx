2ca148343b3efc71c47d0378979f4b03775eff95
paercy32-eng <paercy32@gmail.com>
2026-09-13 13:02:22 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Space10App from './Space10App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Space10App />
+  </React.StrictMode>,
+)
